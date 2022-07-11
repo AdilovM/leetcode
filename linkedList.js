5e6f390945f28ad208dbecedcf47bb93c8adb177
adilovm <miras9191@gmail.com>
2022-07-11 16:25:44 -0700

    append implemented

--- linkedList.js
+++ linkedList.js
@@ -19,6 +19,30 @@ class LinkedList {
     }
     return this;
   }
+
+  append(value) {
+    let newNode = new LinkedListNode(value);
+    if(!this.head || !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+      return this;
+    }
+  }
 }
 
+// let n = new LinkedList();
+// n.prepend(5);
+// n.prepend(6);
+// n.append(7);
+// n.append(8);
+// let temp = n.head;
+// while(temp) {
+//   console.log(temp.value);
+//   temp = temp.next;
+// }
+
+// console.log(n);
 
