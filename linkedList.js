673030c5c295866ea7c81d40224da7026b719749
adilovm <miras9191@gmail.com>
2022-07-11 15:17:41 -0700

    prepend implemented

---
+++ linkedList.js
@@ -0,0 +1,24 @@
+class LinkedListNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  prepend(value) {
+    let newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+    if(!this.tail) {
+      this.tail = newNode;
+    }
+    return this;
+  }
+}
+
+
