549f565715ae73f4c8afd749547e18a659b0edb4
adilovm <miras9191@gmail.com>
2022-09-04 13:29:03 -0700

    completed using iteration

--- roman_to_int.py
+++ roman_to_int.py
@@ -1,22 +1,22 @@
-def roman_to_int(roman):
+def romanToInt(s):
   mapping = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000
   }
-  if len(roman) == 0:
-    return 0
-  elif len(roman) >= 2 and roman[0] == 'C' and (roman[1] == 'M' or roman[1] == 'D'):
-    return mapping[roman[1]] - mapping[roman[0]] + roman_to_int(roman[2:])
-  elif len(roman) >= 2 and roman[0] == 'X' and (roman[1] == 'L' or roman[1] == 'C'):
-    return mapping[roman[1]] - mapping[roman[0]] + roman_to_int(roman[2:])
-  elif len(roman) >= 2 and roman[0] == 'I' and (roman[1] == 'V' or roman[1] == 'X'):
-    return mapping[roman[1]] - mapping[roman[0]] + roman_to_int(roman[2:])
-  else:
-    return mapping[roman[0]] + roman_to_int(roman[1:])
+  result = 0
+  for i in range(len(s) - 1):
+      if mapping[s[i]] < mapping[s[i+1]]:
+          result -= mapping[s[i]]
+      else:
+          result += mapping[s[i]]
 
-print(roman_to_int('III'))
+  result += mapping[s[-1]]
+
+  return result
+print(romanToInt('MCMXCIV'))
+# 621 vs 421
