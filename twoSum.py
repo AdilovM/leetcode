401454ee2b0f0e6b53d96d3271ab8c65bbab87dc
adilovm <miras9191@gmail.com>
2022-09-05 13:09:45 -0700

    refactored using dict

--- twoSum.py
+++ twoSum.py
@@ -5,10 +5,18 @@ class Solution:
     #   for j in nums:
     #     if i + j == target:
     #       return [i, j]
-    for i in range(len(nums)):
-      for j in range(len(nums)):
-        if nums[i] + nums[j] == target:
-          return [i,j]
+    # for i in range(len(nums)):
+    #   for j in range(len(nums)):
+    #     if nums[i] + nums[j] == target:
+    #       return [i,j]
+    map = {}
+    for i in range(0, len(nums)):
+      if target - nums[i] in map:
+        return [map[target - nums[i]], i]
+      else:
+        map[nums[i]] = i
+    return None
   print(twoSum([3,2,4],6))
+  print(twoSum([2,7,11,15], 9))
 
 
