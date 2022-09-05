2eb2c415a3ab16de5b3e178025491f4d2e1dc4a2
adilovm <miras9191@gmail.com>
2022-09-04 19:42:18 -0700

    completed using stack

---
+++ valid_parentheses.py
@@ -0,0 +1,22 @@
+def is_valid(s):
+  closed = {
+  ')' : '(',
+  ']' : '[',
+  '}' : '{'
+  }
+  stack = []
+  for ch in s:
+    if ch not in closed:
+      stack.append(ch)
+    else:
+      if not stack:
+        return False
+      if stack[-1] != closed[ch]:
+        return False
+      stack.pop()
+  if stack:
+    return False
+  else:
+    return True
+
+print(is_valid('()[]'))
